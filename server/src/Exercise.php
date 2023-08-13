ad1b2b123520c8468d4d030581829515d100ff3c
krystiano13 <crystianotv@gmail.com>
2023-08-13 18:19:20 +0200

    update exercise functionality in php

--- server/src/Exercise.php
+++ server/src/Exercise.php
@@ -9,6 +9,7 @@ use App\Connect;
 final class Exercise {
     private string $username;
     private string $plan;
+    private int $id;
 
     private array $exerciseData;
 
@@ -66,6 +67,66 @@ final class Exercise {
             if($sendQuery -> rowCount() <= 0) return array();
             return $sendQuery -> fetchAll();
         } else return false;
+    }
+
+    public function updateExercise() {
+        $connect = new Connect();
+        $connect -> start();
+        
+        $getQuery = $connect -> connection -> prepare(
+            "SELECT * FROM exercises WHERE username=:user AND id=:id"
+        );
+
+        if($getQuery -> execute(array(
+            ':user' => $this -> username,
+            ':id' => $this -> id
+        ))) {
+            if($getQuery -> rowCount() <= 0) return false;
+            $data = $getQuery -> fetchAll();
+
+            $sendQuery = $connect -> connection -> prepare(
+                <<<QUERY
+                UPDATE exercises set
+                plan=:plan,
+                name=:name,
+                sets=:sets,
+                repetitions=:reps,
+                weight=:weight,
+                volume=:volume,
+                progress=:progress
+                WHERE id=:id
+                QUERY
+            );
+            
+            $volume = $data['volume'];
+            $newVolume = $this -> exerciseData['sets'] * $this -> exerciseData['reps'] * $this -> exerciseData['weight'];
+            $progress = $newVolume / $volume * 100;
+            $progressFormated = (float)number_format((float)$progress,2,'.','');
+
+            if(  
+                $sendQuery -> execute(
+                    array(
+                    ":id" => $data['id'],
+                    ":plan" => $this -> plan,
+                    ":name" => $this -> exerciseData['name'],
+                    ":sets" => $this -> exerciseData['sets'],
+                    ":reps" => $this -> exerciseData['reps'],
+                    ":weight" => $this -> exerciseData['weight'],
+                    ":volume" => $this -> $newVolume,
+                    ":progress" => $this -> $progressFormated,
+                ))
+            ) {
+                return true;
+            } else return false;
+
+        }
+        else {
+            return false;
+        }
+    }
+
+    public function setId(int $id) {
+        $this -> id = $id;
     }
 
     public function setUsername(string $name):void {
