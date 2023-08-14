3031896b11c93c5f5d256f0e788a089e3153dd4c
krystiano13 <crystianotv@gmail.com>
2023-08-14 10:30:16 +0200

    create function that gets exercises from database

--- client/src/ReactComponents/Exercises/Exercises.tsx
+++ client/src/ReactComponents/Exercises/Exercises.tsx
@@ -1,16 +1,33 @@
 import type { FunctionComponent } from "preact";
 
 interface ExercisesProps {
-    title: string,
-    username: string
+  title: string;
+  username: string;
 }
 
-const Exercises:FunctionComponent<ExercisesProps> = ({ title, username }) => {
-    return (
-        <main className="PanelContainer">
-
-        </main>
+const Exercises: FunctionComponent<ExercisesProps> = ({ title, username }) => {
+  const getExercisesFromDatabase = async () => {
+    const formData = new FormData();
+    formData.append("plan", title);
+    formData.append("username", username);
+    await fetch(
+      "http://localhost/trainingTracker/server/src/getExercises.php",
+      {
+        method: "post",
+        mode: "cors",
+        body: formData
+      }
     )
-}
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+    return <main className="PanelContainer">
+        <h1>Test</h1>
+      <button className="mt-9" onClick={getExercisesFromDatabase}>Test Api</button>
+  </main>;
+};
 
 export { Exercises };
